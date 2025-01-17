cba54179ac5fb83626af52eb1cea61c75d1505c0
Tuba Balci <tubalci33@gmail.com>
2025-01-17 17:59:55 +0300

    Language switch working

--- src/i18n.js
+++ src/i18n.js
@@ -1,19 +1,18 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import translationEN from './translations/en/global.json';
+import translationTR from './translations/tr/global.json';
 
-import en from './translations/en/global.json';
-import tr from './translations/tr/global.json';
-
-i18n.use(initReactI18next).init({
-  fallbackLng: 'en', // Default language
-  lng: 'en', // Initial language
-  resources: {
-    en: { translation: en },
-    tr: { translation: tr },
-  },
-  interpolation: {
-    escapeValue: false, // React already escapes values
-  },
-});
+i18n
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: { translation: translationEN },
+      tr: { translation: translationTR },
+    },
+    lng: 'en', // default language
+    fallbackLng: 'en',
+    interpolation: { escapeValue: false },
+  });
 
 export default i18n;
