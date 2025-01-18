0ebdb1ad745de363e3a80ae3f6e3cd87624ee597
Tuba Balci <tubalci33@gmail.com>
2025-01-18 14:27:34 +0300

    Fixed all visible problems

--- src/components/Frame.js
+++ src/components/Frame.js
@@ -2,7 +2,11 @@ import React from 'react';
 import styles from './Frame.css'; // Import the scoped CSS
 
 const Frame = ({ children }) => {
-  return <div className={styles["frame-container"]}>{children}</div>;
-};
+  return (
+  
+  <div className={`wrapper`}>
+    <div className={styles["frame-container"]}>{children}</div>
+  </div>
+  )};
 
 export default Frame;
